b7de1ce3c412443ca163e15549cd4379c74a65d1
Ibrahim <ibrahimghali211@gmail.com>
2022-10-01 20:32:29 +0300

    build the backend api

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const pa11y = require('pa11y');
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, console.log(`Server is running on port ${port}...`));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.use(express.static('public'));
+
+app.get('/api/test', async (req, res) => {
+  if (!req.query.url) {
+    res.status(400).json({ error: 'URL is required!' });
+  } else {
+    const result = await pa11y(req.query.url);
+    res.status(200).json(result);
+  }
 });
